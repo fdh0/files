7bcc7e5ca075769600b6dbb5aa64c25789899ca9
FengDonghai <37072652+fdh0@users.noreply.github.com>
2018-08-25 01:02:36 +0800

    Create rename.sh

---
+++ rename.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+name=0
+for files in `ls --file-type *.jpg`
+do
+    # 指定后缀名
+    ext=".jpg"
+    # 指定文件名(这里采用加1的方式)
+    name=$(echo "$name + 1"|bc)
+    # 拼接成完整文件名
+    filename='cat'_$name$ext
+    # 修改文件名
+    mv $files $filename
+done
